ccfc8980bdd3f8dc6aa2464f7780e7cc52769482
Dana Sofía Gómez <sooofixr@gmail.com>
2026-04-22 17:22:15 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,4 +4,12 @@ public class Main {
            TextFileWriter("ejemplo.txt");
         
         try {
-           writer.openFile()
+           writer.openFile();
+           writer.writeToFile("Hola, munto!\n");
+           writer.writeToFile("Este es un archivo de texto plano creado con Java y POO.");
+          System.out.println("Archivo creado exitosamente.");
+        } catch (IOException e) {
+             System .err.println("Error al cerrar el archivo: " + e.getMessage());
+        }
+     }
+}
